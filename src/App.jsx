2b956493c82a9b3e6bc626d6cf1c112d0ec8d048
Puccio <your@email.com>
2025-09-23 13:59:36 -0700

    working in pp revamp

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function Rod() {
   const tipRadius = originalTipRadius * .6;
   const rodRadius = originalRodRadius * .4;
   const tipPositionX = rodLength / 2;
+  
   return (
     <group>
       {/* Rod along X axis */}
@@ -157,16 +158,7 @@ function App() {
   const [currentSeamLon, setCurrentSeamLon] = useState(0);
   const [resetSpin, setResetSpin] = useState(false); // New state for reset trigger
 
-  // useEffect(() => {
-  //   fetch("/averaging_pitch_data.json")
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       setPitches(data || []);
-  //       if (data && data.length > 0) {
-  //         setSelectedPitchUID(data[0].PitchUID);
-  //       }
-  //     });
-  // }, []);
+  
 
     useEffect(() => {
         const handler = (e) => {
